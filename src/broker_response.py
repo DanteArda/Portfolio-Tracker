eaaa88dc80f6b4eae9a22a36ecdb972599a1b881
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2024-10-12 18:27:07 +0100

    Trading212 Methods

--- src/broker_response.py
+++ src/broker_response.py
@@ -1,4 +1,24 @@
 import requests
+from exception import *
+
+DEFAULT_QUERY = {
+    "cursor": None,
+    "ticker": None,
+    "limit": 50,
+}
+
+DEFAULT_PAYLOAD = {
+    "Export csv": {
+        "dataIncluded": {
+            "includeDividends": False,
+            "includeInterest": False,
+            "includeOrders": True,
+            "includeTransactions": False
+        },
+        "timeFrom": 0,
+        "timeTo": 0
+    }
+}
 
 class Broker:
     """
@@ -6,6 +26,40 @@ class Broker:
     """
     def __init__(self, apiKey):
         self.apiKey = apiKey
+
+    def post_response(self, url : str, payload : dict):
+        """
+        Sends a request to perform an action from url
+
+        :param url: Where the payload is sent to
+        :param payload: The request body schema
+        :returns: json parsed as dict
+        """
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.apiKey,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        if response.status_code != 200: raise BadStatusCodeError(response.status_code)
+        return response.json()
+
+    def get_response(self, url : str, query : dict = None) -> dict | BadStatusCodeError:
+        """
+        Sends a request to retrieve data from url
+
+        :param url: Where the request is sent to
+        :param query: any additional parameters required
+        :returns: json parsed as dict
+        """
+
+        headers = {"Authorization": self.apiKey}
+
+        response = requests.get(url, headers=headers, params=query)
+
+        if response.status_code != 200: raise BadStatusCodeError(response.status_code)
+        return response.json()
 
 class Trading212(Broker):
     """
@@ -17,4 +71,50 @@ class Trading212(Broker):
     def __init__(self, apiKey):
         super().__init__(apiKey)
 
-        
+    # Instruments Metadata
+
+    # Pies
+
+    # Equity Orders
+
+    # Account Data
+    def get_account_cash(self):
+        """Fetch all information about available cash, interpolated cash and portfolio value"""
+        return super().get_response("https://live.trading212.com/api/v0/equity/account/cash")
+    
+    def get_account_metadata(self):
+        """Fetch customer account currency"""
+        return super().get_response("https://live.trading212.com/api/v0/equity/account/info")
+
+    # Personal Portfolio
+    def get_all_open_positions(self):
+        """Fetch all open positions for your account"""
+        return super().get_response("https://live.trading212.com/api/v0/equity/portfolio")
+    
+    def get_a_specific_position(self, ticker : str):
+        """Fetch an open position by ticker otherwise just fetch all"""
+        url = "https://live.trading212.com/api/v0/equity/portfolio"
+        if ticker: url += f"/{ticker}"
+
+        return super().get_response(url)
+    
+    # Historical items
+    def get_historical_order_data(self, query : dict = DEFAULT_QUERY):
+        """Get all order history"""
+        return super().get_response("https://live.trading212.com/api/v0/equity/history/orders", query)
+    
+    def get_paid_out_dividends(self, query : dict = DEFAULT_QUERY):
+        """Get all dividend history"""
+        return super().get_response("https://live.trading212.com/api/v0/history/dividends", query)
+    
+    def get_exports_list(self):
+        """Lists detailed information about all csv account exports"""
+        return super().get_response("https://live.trading212.com/api/v0/history/exports")
+    
+    def post_export_csv(self, payload : dict = DEFAULT_PAYLOAD["Export csv"]):
+        """Request a csv export of the account's orders, dividends and transactions history"""
+        return super().post_response("https://live.trading212.com/api/v0/history/exports", payload)
+    
+    def get_transaction_list(self, query : dict = DEFAULT_QUERY):
+        """Fetch superficial information about movements to and from your account"""
+        return super().get_response("https://live.trading212.com/api/v0/history/transactions", query)
---
+++ src/exception.py
@@ -0,0 +1,18 @@
+class BadStatusCodeError(Exception):
+    """Returns an error if the API response returns a status code other than 200"""
+    def __init__(self, status_code : int):
+        def return_status_code_message():
+            match self.status_code:
+                case 200: return "OK; You most likely did not intend for this to happen"
+                case 401: return "Bad API key"
+                case 403: return "Scope missing for API key"
+                case 408: return "Timed-Out"
+                case 429: return "Limited"
+                case _: return "Unknown"
+
+        self.status_code = status_code
+        self.message = return_status_code_message()
+
+        super().__init__(f"{self.status_code}: {self.message}")
+
+    
