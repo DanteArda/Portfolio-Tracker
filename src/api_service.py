2281c5a4820b56e2380445abc22f45e447ce48ba
Dante <dominic.ardavicius.2023@live.rhul.ac.uk>
2024-11-29 18:44:43 +0000

    post requests

--- src/api_service.py
+++ src/api_service.py
@@ -22,31 +22,43 @@ class Trading212:
         """Fetch all open positions."""
         return self.request("GET", "/api/v0/equity/portfolio")
     
+    def search_for_a_specific_position_by_ticker(self, ticker):
+        """Search for an open position by ticker."""
+        return self.request("POST", "/api/v0/equity/portfolio/ticker", ticker)
+    
     def fetch_a_specifc_position(self, ticker : str):
         """Fetch an open position by ticker."""
         return self.request("GET", f"/api/v0/equity/portfolio/{ticker}")
 
     # API
-    def request(self, method, path) -> dict:
+    def request(self, method : str, path : str, body : str) -> dict:
         """
         Request data from an API endpoint.
     
         Args:
             method (str): Whether to send a GET or POST request.
             path (str): path to the API endpoint.
+            body (str): additional parameters to be sent.
 
         Returns:
             dict: json object with request data, or nothing if an error.
         """
         url = self.base_url + path
 
-        headers = {"Authorization" : self.API_key}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization" : self.API_key
+            }
 
         if method == "GET":
             response = requests.get(url, headers=headers)
 
         elif method == "POST":
-            pass
+            payload = {
+                "ticker" : body
+            }
+
+            response = requests.post(url, json=payload, headers=headers)
 
         else:
             pass
