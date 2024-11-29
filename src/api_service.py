a5229fa92c48cf626cb5ff857a5bce63e07cc37f
Dante <dominic.ardavicius.2023@live.rhul.ac.uk>
2024-11-29 19:35:32 +0000

    historical item methods

--- src/api_service.py
+++ src/api_service.py
@@ -5,6 +5,30 @@ class Trading212:
     http://trading212.com"""
     base_url = "https://live.trading212.com"
 
+    DEFAULT_QUERY_PARAMETERS = {
+        "cursor" : "0",
+        "ticker" : None,
+        "limit" : 50
+    }
+
+    # TODO change this varname
+    DEFAULT_QUERY_PARAMETERS_TRANSACTION = {
+        "cursor" : "0",
+        "time" : "2000-01-01 00:00:00",
+        "limit" : 50
+    }
+
+    DEFAULT_BODY_SCHMEA = {
+        "dataIncluded" : {
+            "includeDividends" : True,
+            "includeInterest" : True,
+            "includeOrders" : True,
+            "includeTransactions" : True 
+        },
+        "timeFrom" : "2000-01-01 00:00:00",
+        "timeTo" : "3000-01-01 00:00:00"
+    }
+
     def __init__(self, API_key):
         self.API_key = API_key
 
@@ -24,21 +48,43 @@ class Trading212:
     
     def search_for_a_specific_position_by_ticker(self, ticker):
         """Search for an open position by ticker."""
-        return self.request("POST", "/api/v0/equity/portfolio/ticker", ticker)
+        body = {"ticker" : ticker}
+        return self.request("POST", "/api/v0/equity/portfolio/ticker", body)
     
     def fetch_a_specifc_position(self, ticker : str):
         """Fetch an open position by ticker."""
         return self.request("GET", f"/api/v0/equity/portfolio/{ticker}")
 
+    # Historical Items
+    def historical_order_data(self, query : dict = DEFAULT_QUERY_PARAMETERS):
+        """Fetch all historical order data for positions."""
+        return self.request("GET", "/api/v0/equity/history/orders", query)
+
+    def paid_out_dividends(self, query : dict = DEFAULT_QUERY_PARAMETERS):
+        """Fetch all historical dividend payments."""
+        return self.request("GET", "/api/v0/history/dividends", query)
+
+    def exports_list(self):
+        """List detailed information about all csv account exports."""
+        return self.request("GET", "/api/v0/history/exports")
+
+    def export_csv(self, query : dict = DEFAULT_QUERY_PARAMETERS):
+        """Request a csv export of the account's orders, dividends and transactions history"""
+        return self.request("POST", "/api/v0/history/exports", query)
+
+    def transaction_list(self, query : dict = DEFAULT_QUERY_PARAMETERS_TRANSACTION):
+        """Return monetary movements in and out of the account"""
+        return self.request("GET", "/api/v0/history/transactions", query)
+
     # API
-    def request(self, method : str, path : str, body : str) -> dict:
+    def request(self, method : str, path : str, body : dict = None) -> dict:
         """
         Request data from an API endpoint.
     
         Args:
             method (str): Whether to send a GET or POST request.
             path (str): path to the API endpoint.
-            body (str): additional parameters to be sent.
+            body (dict): additional parameters to be sent.
 
         Returns:
             dict: json object with request data, or nothing if an error.
@@ -51,14 +97,10 @@ class Trading212:
             }
 
         if method == "GET":
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers, params=body)
 
         elif method == "POST":
-            payload = {
-                "ticker" : body
-            }
-
-            response = requests.post(url, json=payload, headers=headers)
+            response = requests.post(url, json=body, headers=headers)
 
         else:
             pass
