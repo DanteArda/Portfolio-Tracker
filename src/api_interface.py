88585a54f3e9b590466714316a5b8198c3fa536d
Dante <dominic.ardavicius.2023@live.rhul.ac.uk>
2024-11-30 18:46:19 +0000

    write to cash attributes

--- src/api_interface.py
+++ src/api_interface.py
@@ -1,11 +1,17 @@
 import api_service
-import portfolio_generator
+from portfolio_generator import Portfolio
 
 class Hook:
-    def __init__(self, broker_api : api_service, portfolio : portfolio_generator.Portfolio):
+    def __init__(self, broker_api : "api_service", portfolio : "Portfolio"):
         self.broker_api = broker_api
         self.portfolio = portfolio 
 
-    
-    
+    def write_cash(self):
+        """Read and Write from broker api to the Portfolio class."""
+        cash_dict = Portfolio.filter_new_cash_attributes()
 
+        # TODO
+        # write from api to cash_dict
+
+        self.portfolio.write_to(self.portfolio.Cash, cash_dict)
+        
--- src/portfolio_generator.py
+++ src/portfolio_generator.py
@@ -1,6 +1,46 @@
 class Portfolio:
     """General Interface for an Investment Portfolio."""
 
+    # Attributes
+    @staticmethod
+    def filter_attributes(cls : object) -> dict:
+        """
+        Filters through __dict__, removing any key value that begins with '_'; therefore removing internal attributes.
+
+        Args:
+            cls (object): A general object.
+
+        Returns:
+            dict: The filtered attributes.
+        """
+        return {key : value for key, value in cls.__dict__.items() if not key.startswith('_')}
+
+    # Attributes - Cash
+    def filter_cash_attributes(self) -> dict:
+        """Returns filter_attributes of calling instance."""
+        return Portfolio.filter_attributes(self.Cash)
+
+    @staticmethod
+    def filter_new_cash_attributes() -> dict:
+        """Returns filter_attributes with Portfolio.Cash as parameters."""
+        return Portfolio.filter_attributes(Portfolio.Cash)
+    
+    # Attributes - Setting
+    @staticmethod
+    def write_to(cls : object, data : dict):
+        """
+        Overwrites the attributes in a class.
+
+        The data to overwrite can be retrieved by using a filter method, e.g. filter_new_cash_attributes to retrieve a template for the Cash attributes.
+
+        Args:
+            cls (object): The attributes of the class to be overwritten.
+            data (dict): The data to be flushed onto the attributes.
+        """
+        for key, value in data.items():
+            if hasattr(cls, key):
+                setattr(cls, key, value)
+
     class Cash:
         """
         Interface for portfolio balances.
@@ -12,6 +52,7 @@ class Portfolio:
             profit_and_loss (int): Current profit and loss on all open positions.
             realized (int): Total realized profit or loss.
             total (int): Total cash held after price movements.
+            total_dividend (int): Total interest from dividend payments.
         """
         blocked = 0
         free = 0
@@ -19,4 +60,4 @@ class Portfolio:
         profit_and_loss = 0
         realized = 0
         total = 0
-
+        total_dividend = 0
