292f88df1b1eb051beede9e676f577e2bba6300c
Dante <dominic.ardavicius.2023@live.rhul.ac.uk>
2024-12-14 18:57:04 +0000

    able to get api key

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 
-class ConfigAPI:
+class Config:
     """An Interface for making it incredibly easy to read and write from a .ini config file."""
 
     cur_dir = os.path.dirname(os.path.abspath(__file__))
@@ -20,19 +20,32 @@ class ConfigAPI:
             os.makedirs(self.local_dir)
             with open(self.config_file_dir, 'w'): pass
 
-    def set(self, platform : str, value : str, option : str = "API_key"):
+    def set(self, platform : str, value : str, name : str = "API_key"):
         """
         Set the key associated to the platform.
 
         Args:
             platform (str): The trading platfrom the key belongs to.
             value (str): The key value.
-            option (str): The key name, default is "API_key".
+            name (str): The key name, default is "API_key".
         """
         if not self.has_platform(platform):
             self.config.add_section(platform)
         
-        self.config.set(platform, option, value)
+        self.config.set(platform, name, value)
+
+    def get(self, platform : str, fetch : str = "API_key") -> str:
+        """
+        Get the key for a specified trading platform.
+
+        Args:
+            platform (str): The trading platform to search for.
+            fetch (str): The specified key name, default is 'API_KEY'
+
+        Returns:
+            str: The API key, nothing if not found.
+        """
+        return self.config.get(platform, fetch)
 
     def has_platform(self, platform : str):
         """
@@ -50,5 +63,3 @@ class ConfigAPI:
         """Save the configuration to a file."""
         with open(self.config_file_dir, 'w') as config_file:
             self.config.write(config_file)
-
-    
